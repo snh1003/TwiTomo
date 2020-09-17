a76d4a00760508ca92fa545b6c393957dd8db66c
snh1003 <lunasaprismriver57@gmail.com>
2020-09-17 23:39:35 +0900

    add Platform px size in theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,9 @@
 //색은 미정 테마 칼라 정하고 수정, 아무거나해둠
+const  size = {
+  mobile: "767px",
+  desktop: "1281px"
+}
+
 const theme = {
   main: {
     base: "#ebecf0",
@@ -10,6 +15,9 @@ const theme = {
     bright: "#c46476",
     dark: "#600e1e",
   },
+  Platform: {
+    mobile: `(max-width: ${size.mobile}`
+  }
 };
 
 export default theme;
