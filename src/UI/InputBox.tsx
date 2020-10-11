5b0a094b236050e35c8de5dc43663f5b1749c6b3
snh1003 <lunasaprismriver57@gmail.com>
2020-10-11 23:47:20 +0900

    InputBox.tsx props

--- src/UI/InputBox.tsx
+++ src/UI/InputBox.tsx
@@ -2,46 +2,49 @@ import React from "react";
 import styled from "styled-components";
 
 interface InputBoxProps {
-  value?: string;
   width?: string;
   height?: string;
   borderRadius?: string;
+  fontsize?: string;
   margin?: string;
-  Type?: string;
-  name?: string;
+  padding?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 }
 
 const StyledInput = styled.input<
-  Pick<InputBoxProps, "width" | "height" | "borderRadius" | "margin">
+  Pick<
+    InputBoxProps,
+    "padding" | "width" | "height" | "borderRadius" | "margin" | "fontsize"
+  >
 >`
-  padding: 1rem;
+  font-size: ${({ fontsize }) => fontsize};
+  padding: ${({ padding }) => padding};
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   border-radius: ${({ borderRadius }) => borderRadius};
   margin-top: ${({ margin }) => margin};
   margin-bottom: ${({ margin }) => margin};
-`;
-
-const StyledBox = styled.article`
-  float: left;
-  display: inline;
+  text-align: center;
 `;
 
 const InputBox: React.FC<InputBoxProps> = ({
-  value,
   width,
   height,
   borderRadius,
+  fontsize,
+  padding,
   margin,
-  Type,
   onChange,
   onKeyPress,
+  placeholder,
 }) => {
   return (
     <StyledInput
-      placeholder="kim"
+      placeholder={placeholder}
+      padding={padding}
+      fontsize={fontsize}
       width={width}
       height={height}
       margin={margin}
