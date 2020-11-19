4f0cd7c5cbb3ce34d49ce6aba9353425bc60e158
snh1003 <37822075+snh1003@users.noreply.github.com>
2020-11-19 10:00:07 +0900

    Update Intersection.tsx

--- src/Hooks/Intersection.tsx
+++ src/Hooks/Intersection.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-
+//공부용 코드 미완성
+//Intersection Observer api문서를 읽고 완성할것
+//인피니티 스크롤에 이용하는것이 당면의 목표 
 export const useIntersection = (ref) => {
   const [intersecting, setIntersecting] = React.useState(false);
 
